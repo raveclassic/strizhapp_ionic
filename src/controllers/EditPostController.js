6f589b8b8a257ef3101011eb6914ab64dfb69be0
raveclassic <raveclassic@live.ru>
2015-05-23 12:17:26 +0400

    post editing

--- src/controllers/EditPostController.js
+++ src/controllers/EditPostController.js
@@ -2,30 +2,43 @@ import Post from '../models/Post.js';
 
 export default function EditPostController($scope, post, $ionicLoading, $ionicPopup, ApiService, $state, $ionicHistory) {
 	window.$ionicHistory = $ionicHistory;
-	$scope.post = post;
-	//
-	//$scope.save = function() {
-	//	let focused = document.querySelector('form input:focus, form textarea:focus');
-	//	if (focused) {
-	//		focused.blur();
-	//	}
-	//
-	//	$ionicLoading.show();
-	//	$scope.newPost.real_price = $scope.newPost.price;
-	//	ApiService.post('post', $scope.newPost)
-	//		.then(() => {
-	//			$state.go('app.home.posts');
-	//		})
-	//		.catch((error) => {
-	//			$ionicPopup.alert({
-	//				title: error.data.name + ' ' + error.data.status,
-	//				template: error.data.message
-	//			});
-	//		})
-	//		.finally(() => {
-	//			$ionicLoading.hide();
-	//		});
-	//};
+	$scope.post = angular.copy(post);
+
+	$scope.save = function () {
+		let focused = document.querySelector('form input:focus, form textarea:focus');
+		if (focused) {
+			focused.blur();
+		}
+
+		$ionicLoading.show();
+		Post.update($scope.post.id, $scope.post)
+			.then(() => {
+				$state.go('app.home.posts');
+			})
+			.catch(error => {
+				$ionicPopup.alert({
+					title: error.data.name + ' ' + error.data.status,
+					template: error.data.message
+				});
+			})
+			.finally(() => {
+				$ionicLoading.hide();
+			});
+		//	$scope.newPost.real_price = $scope.newPost.price;
+		//	ApiService.post('post', $scope.newPost)
+		//		.then(() => {
+		//			$state.go('app.home.posts');
+		//		})
+		//		.catch((error) => {
+		//			$ionicPopup.alert({
+		//				title: error.data.name + ' ' + error.data.status,
+		//				template: error.data.message
+		//			});
+		//		})
+		//		.finally(() => {
+		//			$ionicLoading.hide();
+		//		});
+	};
 
 	$scope['delete'] = function () {
 		$ionicLoading.show();
@@ -33,7 +46,7 @@ export default function EditPostController($scope, post, $ionicLoading, $ionicPo
 			.then(() => {
 				try {
 					$ionicHistory.goBack(-2);
-				} catch(e) {
+				} catch (e) {
 					$state.go('app.home.posts');
 				}
 			})
